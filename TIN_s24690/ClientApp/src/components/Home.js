9dae89e92d1bfc97d2c68ebb71e0ce8b70c96544
Julia <julia.szefer@gmail.com>
2024-01-23 01:36:18 +0100

    Home layout

--- TIN_s24690/ClientApp/src/components/Home.js
+++ TIN_s24690/ClientApp/src/components/Home.js
@@ -9,7 +9,21 @@ export default function Home(){
             <button>MENU</button>
             <button>ZALOGUJ</button>
             <button>KOSZYK</button>
+            <button>EN</button>
         </header>
+        <div className={"menuContainer"}>
+            <div className={"menuBox"}>
+                <div className={"productName"}>
+
+                </div>
+                <div className={"productList"}>
+
+                </div>
+            </div>
+        </div>  
       </div>
     );
 }
+
+//jesli zalogowany uzytkownik: HISTORIA ZAMOWIEN + KONTO
+//jesli zalogowany administrator: ZARZADZAJ
